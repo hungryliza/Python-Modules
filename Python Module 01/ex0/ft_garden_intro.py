719c2c534527e603e61e039673023d1fd3fe3d58
hungryliza <53476586+hungryliza@users.noreply.github.com>
2026-04-01 00:55:09 +0100

    Add files via upload

---
+++ Python Module 01/ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    print("=== Welcome to My Garden ===")
+    name = "Rose"
+    height = "25cm"
+    age = "30 days"
+    print("Name :", name)
+    print("Height :", height)
+    print("Age :", age)
+    print("=== End of Program ===")
